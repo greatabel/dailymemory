da5868255d7a1afcc54cdd54bdc3a373eadbe947
abel <abel.wan@31huiyi.com>
2014-12-02 15:37:29 +0800

    content

--- javascript/bootstrap-tips-hierarchy-check.js
+++ javascript/bootstrap-tips-hierarchy-check.js
@@ -1,1 +1,6 @@
 
+    $(document).on('change', '.joinCateAll', function ()
+    {
+        var chked = $(this).attr("checked") == "checked" ? true : false;
+        $(this).closest(".checkbox").next().find("input[type=checkbox]").checkbox(chked?"check":"uncheck" );
+    });
