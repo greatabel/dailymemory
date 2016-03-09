f76acb2e20e1002c72d6644312cde6aa45104bd0
greatabel <myreceiver2for2github@gmail.com>
2016-03-09 10:39:12 +0800

    js & node

--- javascript/bootstrap-tips-hierarchy-check.js
+++
@@ -1,6 +0,0 @@
-//连带选择bootstrap的checkbox
-    $(document).on('change', '.joinCateAll', function ()
-    {
-        var chked = $(this).attr("checked") == "checked" ? true : false;
-        $(this).closest(".checkbox").next().find("input[type=checkbox]").checkbox(chked?"check":"uncheck" );
-    });
